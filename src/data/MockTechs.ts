6b07a92ab652900bea5ceecea3be3f7a5c9f9337
WesRamox <wesleyramox258@gmail.com>
2024-06-16 11:32:13 -0300

    Skills description

--- src/data/MockTechs.ts
+++ src/data/MockTechs.ts
@@ -5,56 +5,56 @@ export function useTechs() {
       {
          id: 0,
          name: "HTML5",
-         description: "...",
-         experience: "2 years",
+         description: "A Linguagem de Marcação de Hipertexto (HTML) é uma linguagem de computador que compõe a maior parte das páginas da internet e dos aplicativos online. Um hipertexto é um texto usado para fazer referência a outros textos, enquanto uma linguagem de marcação é composta por uma série de marcações que dizem para os servidores da web qual é o estilo e a estrutura de um documento.",
+         experience: "2 anos",
       },
       {
          id: 1,
          name: "CSS",
-         description: "...",
-         experience: "2 years"
+         description: "CSS é chamado de linguagem Cascading Style Sheet e é usado para estilizar elementos escritos em uma linguagem de marcação como HTML. O CSS separa o conteúdo da representação visual do site.",
+         experience: "2 anos"
       },
       {
          id: 2,
          name: "BOOTSTRAP",
-         description: "...",
-         experience: "2 years"
+         description: "Bootstrap é um framework front-end que fornece estruturas de CSS para a criação de sites e aplicações responsivas de forma rápida e simples. Além disso, pode lidar com sites de desktop e páginas de dispositivos móveis da mesma forma.",
+         experience: "1 ano"
       },
       {
          id: 3,
          name: "GIT",
-         description: "...",
-         experience: "2 years"
+         description: "O Git é um sistema de controle de versão de arquivos; em outras palavras, é responsável por guardar o histórico de alterações sempre que alguém modificar algum arquivo que está sendo monitorado por ele.",
+         experience: "1 ano"
       },
       {
          id: 4,
          name: "GITHUB",
-         description: "...",
-         experience: "2 years"
+         description: "o GitHub é um serviço baseado em nuvem que hospeda um sistema de controle de versão (VCS) chamado Git. Ele permite que os desenvolvedores colaborem e façam mudanças em projetos compartilhados enquanto mantêm um registro detalhado do seu progresso.",
+         experience: "2 anos"
       },
       {
          id: 5,
          name: "REACT",
-         description: "...",
-         experience: "2 years"
+         description: "React é um framework JavaScript criado pelo Facebook (atual Meta) que é usado para criar interfaces de usuário (UI) em aplicativos web. Ele é popular por ser fácil de usar, altamente flexível e escalável, e é usado por muitas empresas de tecnologia, incluindo o Facebook, Instagram e Airbnb.",
+         experience: "2 meses"
       },
       {
          id: 6,
          name: "JAVASCRIPT",
-         description: "...",
-         experience: "2 years"
+         description: "JavaScript ou JS é uma linguagem de programação projetada para escrever as partes de Front-end (lado do cliente) e Back-end (lado do servidor) dos sites e aplicativos. É uma linguagem de alto nível, legível por humanos, mas que não necessita de um compilador para traduzi-la para a linguagem de máquina.",
+         experience: "1 ano"
       },
       {
          id: 7,
          name: "TAILWIND",
-         description: "...",
-         experience: "2 years"
+         description: "Tailwind é um framework CSS que permite há você criar layouts para suas aplicações web utilizando sua estrutura. Ou seja, uma ferramenta que fornece componentes para sua estilização. O propósito geral da criação desse framework é aumentar a velocidade para o desenvolvimento, agilizando os processos de criação de seus web sites.",
+         experience: "3 meses"
       },
       {
          id: 8,
          name: "TYPESCRIPT",
-         description: "...",
-         experience: "2 years"
+         description: "TypeScript é um superconjunto de JavaScript, ou seja, um conjunto de ferramentas e formas mais eficientes de escrever código JavaScript, adicionando recursos que não estão presentes de maneira nativa na linguagem.",
+         experience: "1 mês"
       },
    ];
 
