1362e703e2a3ed03216bfa9712cad99e204b1ee4
Wesley Ramos <wesleyramox258@gmail.com>
2024-09-13 19:56:15 -0300

    Update Job.tsx

--- src/components/Job/Job.tsx
+++ src/components/Job/Job.tsx
@@ -7,7 +7,7 @@ export default function Job() {
     <div className="flex gap-3">
       <div className="flex flex-col justify-around">
         <PiDevicesDuotone size={30} />
-        <SiFreelancer size={30} />
+{/*         <SiFreelancer size={30} /> */}
       </div>
       <div className="exp flex flex-col gap-3">
         <div className="flex flex-col text-start border-l-[1px] pl-2 border-blue-600">
@@ -17,13 +17,13 @@ export default function Job() {
             <span className="dark:text-zinc-400 text-gray-500 italic max-sm:text-md max-sm:text-sm">07/2024 - Atual</span>
           </h3>
         </div>
-        <div className="flex flex-col text-start border-l-[1px] pl-2 border-blue-300">
+{/*         <div className="flex flex-col text-start border-l-[1px] pl-2 border-blue-300">
           <h2 className="text-2xl dark:text-zinc-300 text-zinc-700 max-sm:text-md max-sm:text-sm">Desenvolvedor Full Stack</h2>
           <h3 className="flex gap-2 text-xl" >
             <span className="dark:text-blue-300 text-blue-500 max-sm:text-md max-sm:text-sm">Freelancer</span>
             <span className="dark:text-zinc-400 text-gray-500 italic max-sm:text-md max-sm:text-sm">12/2023 - Atual</span>
           </h3>
-        </div>       
+        </div>        */}
       </div>
     </div>
     
